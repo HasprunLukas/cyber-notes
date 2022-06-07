cda1b40a4a0e52aee8405adf7b254b7d397df7ae
hasprun.lukas <hasprun.lukas@gmail.com>
2022-06-07 23:05:43 +0200

    First Commit

---
+++ cyber-notes-spring/src/main/java/sk/cybersoft/cybernotes/cybernotesspring/CyberNotesSpringApplication.java
@@ -0,0 +1,13 @@
+package sk.cybersoft.cybernotes.cybernotesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberNotesSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberNotesSpringApplication.class, args);
+    }
+
+}
---
+++ cyber-notes-spring/src/test/java/sk/cybersoft/cybernotes/cybernotesspring/CyberNotesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.cybersoft.cybernotes.cybernotesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberNotesSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
