f6a243a6c743b2b17b36ae2efa7f1c393bda9192
hasprun.lukas <hasprun.lukas@gmail.com>
2022-06-22 00:12:23 +0200

    Ongoing work

--- cyber-notes-spring/src/test/java/sk/cybersoft/cybernotes/cybernotesspring/controller/AccountControllerTest.java
+++ cyber-notes-spring/src/test/java/sk/cybersoft/cybernotes/cybernotesspring/controller/AccountControllerTest.java
@@ -1,37 +1,20 @@
 package sk.cybersoft.cybernotes.cybernotesspring.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import sk.cybersoft.cybernotes.cybernotesspring.CyberNotesSpringApplication;
 import sk.cybersoft.cybernotes.cybernotesspring.entity.AccountEntity;
-import sk.cybersoft.cybernotes.cybernotesspring.repository.AccountRepository;
-import sk.cybersoft.cybernotes.cybernotesspring.service.AccountService;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-
-import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = CyberNotesSpringApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
